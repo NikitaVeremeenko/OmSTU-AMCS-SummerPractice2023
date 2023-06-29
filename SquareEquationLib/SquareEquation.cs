6463b565df02f672d8db4b85121cc966c75781c8
NikitaVeremeenko <nikitaveremeenko03@gmail.com>
2023-06-29 16:57:57 +0600

    task2

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,6 +4,31 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        throw new NotImplementedException();
+        double eps = 0.000000001;
+        double[] Roots_Of_The_Equation;
+
+        if (Math.Abs(a) < eps || new[] { a, b, c }.Any(double.IsNaN) || new[] { a, b, c }.Any(double.IsInfinity))
+        {
+            throw new ArgumentException();
+        }
+
+        double d = b * b - 4 * a * c;
+
+        if (d < 0 && !(Math.Abs(d) < eps))
+        {
+            Roots_Of_The_Equation = new double[0];
+        }
+        else if (d < eps)
+        {
+            Roots_Of_The_Equation = new double[1];
+            Roots_Of_The_Equation[0] = -(b + Math.Sign(b) * Math.Sqrt(d)) / 2;
+        }
+        else
+        {
+            Roots_Of_The_Equation = new double[2];
+            Roots_Of_The_Equation[0] = -(b + Math.Sign(b) * Math.Sqrt(d)) / 2;
+            Roots_Of_The_Equation[1] = c / Roots_Of_The_Equation[0];
+        }
+        return Roots_Of_The_Equation;
     }
 }
